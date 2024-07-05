d110b1cba3fc03cf58d39c8ec2b1bd20c69649f3
Bimzy <branden.immerzeel@gmail.com>
2024-07-05 16:24:56 +0930

    WIP calculator

--- TextBasedCalculator/TextBasedCalculator/TextBasedCalculator.cpp
+++ TextBasedCalculator/TextBasedCalculator/TextBasedCalculator.cpp
@@ -3,13 +3,74 @@
 
 #include <iostream>
 #include <string>
+#include <queue>
+#include <set>
 
 class Calculator
 {
 public:
     int Calculate(std::string input)
     {
-        return 69;
+        std::set<char> operands = {'+', '-', '*', '/'};
+        std::queue<std::string> equation;
+
+        //Create equation from string
+        std::string curNum = "";
+        while (input.length() > 0)
+        {
+            char c = input[0];
+            input.erase(input.begin());
+
+            if (isdigit(c))
+            {
+                curNum += c;
+            }
+            else if (operands.find(c) != operands.end())
+            {
+                equation.push(curNum);
+                std::string str(1, c);
+                equation.push(str);
+                curNum = "";
+            }
+        }
+
+        float result = 0;
+        char op = '+';
+
+        //Calculate equation
+        while (!equation.empty())
+        {
+            if (operands.find(equation.front()[0]) != operands.end()) // is operator character
+            {
+                op = equation.front()[0];
+            }
+            else
+            {
+                float f = std::stof(equation.front());
+                if (op == '+')
+                {
+                    result += f;
+                }
+                else if (op == '-')
+                {
+                    result -= f;
+                }
+                else if (op == '*')
+                {
+                    result *= f;
+                }
+                else if (op == '/')
+                {
+                    result /= f;
+                }
+
+                std::cout << "result: " << result << " op: " << op << "\n";
+            }
+
+            equation.pop();
+        }
+
+        return result;
     }
 };
 
